565c79a2fa0f65aa32e7bb8be20fabf1c7426841
Shidlingayya <65803331+shidlingayya@users.noreply.github.com>
2022-04-21 21:34:26 +0530

    Add files via upload

---
+++ dl_InsertNodeAtLast.cpp
@@ -0,0 +1,97 @@
+#include <iostream> 
+struct doublyLinkedList
+{
+	struct doublyLinkedList* prev;
+	int data;
+	struct doublyLinkedList* next;
+};
+
+class DLinkedList
+{
+public:
+	DLinkedList():head(nullptr){}
+	void insertNodeFront(int value)
+	{
+		//Create new node with given value
+		doublyLinkedList* new_node = new doublyLinkedList;
+		if (new_node != nullptr)
+		{
+			new_node->prev = nullptr;
+			new_node->data = value;
+			new_node->next = nullptr;
+		}
+
+		//If list is empty
+		if (head == nullptr)
+		{
+			head = new_node;
+		}
+		else
+		{
+			new_node->next = head;
+			head = new_node;
+		}
+	}
+
+	void insertNodeRear(int value)
+	{
+		//Create new node with given value
+		doublyLinkedList* new_node = new doublyLinkedList;
+		if (new_node != nullptr)
+		{
+			new_node->prev = nullptr;
+			new_node->data = value;
+			new_node->next = nullptr;
+		}
+
+		//If list is empty
+		if (head == nullptr)
+		{
+			head = new_node;
+		}
+		else
+		{
+			doublyLinkedList* copy_of_head = head;
+			while (copy_of_head->next != nullptr)
+			{
+				copy_of_head = copy_of_head->next;
+			}
+
+			//Now copy new node to last node pointer
+			copy_of_head->next = new_node;
+		}
+	}
+
+	void display()
+	{
+		if (head == nullptr)
+		{
+			std::cout << "List is empty" << std::endl;
+			return;
+		}
+
+		doublyLinkedList* copy_of_head = head;
+		while (copy_of_head != nullptr)
+		{
+			std::cout << copy_of_head->data << "->";
+			copy_of_head = copy_of_head->next;
+		}
+		std::cout << std::endl;
+	}
+private:
+	doublyLinkedList* head;
+};
+
+int main() 
+{
+	DLinkedList dll;
+	dll.insertNodeFront(10);
+	dll.insertNodeFront(30);
+	dll.insertNodeFront(5);
+	dll.insertNodeFront(6);
+	dll.display();
+	std::cout << "Insert node at rear end" << std::endl;
+	dll.insertNodeRear(6);
+	dll.display();
+	return 0;
+}
